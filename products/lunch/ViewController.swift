b11c817f1ea79cd72afbcfe060b35efc3cee52e3
岩橋 聡吾 <g00101@wp3n-207.cyberagent.co.jp>
2017-05-09 13:05:54 +0900

    basesources commited

---
+++ products/lunch/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  lunch
+//
+//  Created by 岩橋 聡吾 on 2017/05/09.
+//  Copyright © 2017年 swifters. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
